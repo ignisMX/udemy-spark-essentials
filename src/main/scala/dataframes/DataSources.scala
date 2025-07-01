21c79ec6630518f33a4620e295304b034b2ad0e9
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2025-07-01 22:16:59 +0000

    it adds flags concepts for json, csv, text file and parquet.

--- src/main/scala/dataframes/DataSources.scala
+++ src/main/scala/dataframes/DataSources.scala
@@ -64,4 +64,55 @@ object DataSources extends App {
     .format("json")
     .mode(SaveMode.Overwrite)
     .save("src/main/scala/resources/data/cars_duplicated.json")
+
+  // JSON Flags
+  spark.read
+    .schema(carsSchema)
+    .option("dateFormat", "yyyy-MM-dd") // couple with schema; if Spark fails parsing, it will put null
+    .option("allowSingleQuotes", "true")
+    .option("compression", "uncompressed") // bzip, gzip, lz4, snappy, deflated
+    .json("src/main/scala/resources/data/cars.json")
+
+  // CSV Flags
+  val stockSchema = StructType(
+    Array(
+      StructField("symbol", StringType),
+      StructField("date", DateType),
+      StructField("price", DoubleType)
+    )
+  )
+
+  spark.read
+    .schema(stockSchema)
+    .option("dateFormat","MMM d yyyy")
+    .option("header","true")
+    .option("sep",",")
+    .option("nullValue","")
+    .csv("src/main/scala/resources/data/stocks.csv").show()
+
+  // parquet
+  carsDF.write
+    .mode(SaveMode.Overwrite)
+    .save("src/main/scala/resources/data/cars.parquet")
+
+  // Text files
+  spark.read.text("src/main/scala/resources/data/sampleTextFile.txt").show()
+
+  // Reading from a remote DB
+  val driver ="org.postgresql.Driver"
+  val url = "jdbc:postgresql://localhost:5433/rtjvm"
+  val user = "docker"
+  val password = "docker"
+
+  val employeesDF=spark.read
+    .format("jdbc")
+    .option("driver",driver)
+    .option("url",url)
+    .option("user", user)
+    .option("password", password)
+    .option("dbtable","public.employees")
+    .load()
+
+  employeesDF.show()
+
 }
