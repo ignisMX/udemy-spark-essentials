7519b160e4e6aa175c61a8f74a177ebcadfaa2a7
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2025-07-13 06:42:01 +0000

    it adds exercises for data sets

--- src/main/scala/typesdatasets/Datasets.scala
+++ src/main/scala/typesdatasets/Datasets.scala
@@ -1,6 +1,7 @@
 package typesdatasets
 
 import org.apache.spark.sql.{Encoders, SparkSession, Dataset}
+import org.apache.spark.sql.functions._
 
 object Datasets extends App {
   val spark = SparkSession.builder()
@@ -34,14 +35,16 @@ object Datasets extends App {
                 )
 
   // 2 - read the data frame from the file
-  def readDataFrame(filename:String) = spark.read
-    .option("inferSchema","true")
+  def readDataFrame(filename: String) = spark.read
+    .option("inferSchema", "true")
     .json(s"src/main/scala/resources/data/$filename")
 
   val carsDataFrame = readDataFrame("cars.json")
 
   // 3 - define an encoder (importing the implicits)
+
   import spark.implicits._
+
   var carsDataset = carsDataFrame.as[Car]
   carsDataset.show(false)
 
@@ -51,4 +54,26 @@ object Datasets extends App {
   // map, flatMap, fold. reduce, for comprehensions ...
   val carNamesDataset = carsDataset.map(car => car.Name.toUpperCase())
   carNamesDataset.show(false)
+
+  /**
+   * Exercises
+   *
+   * 1. Count how many cars we have
+   * 2. Count how many POWERFUL cars we have (HP > 140)
+   * 3. Average HP for the entire dataset
+   */
+
+  val numberOfCars = carsDataset.count()
+  println(s"number of cars: $numberOfCars")
+
+  val numberOfPowerfulCars = carsDataset.filter(car => car.Horsepower.getOrElse(0L) > 140).count()
+  println(s"number of powerful cars: $numberOfPowerfulCars")
+
+  val sumOfHorsepower: Long = carsDataset.map(_.Horsepower.getOrElse(0L)).reduce(_ + _)
+  val averageHorsepower: Long = sumOfHorsepower / carsDataset.count()
+  println(s"avergae horsepower $averageHorsepower")
+
+  // data frames function
+  val averageHorsepowerWithDataFrameFunction = carsDataset.select(avg(column("Horsepower")))
+  averageHorsepowerWithDataFrameFunction.show()
 }
