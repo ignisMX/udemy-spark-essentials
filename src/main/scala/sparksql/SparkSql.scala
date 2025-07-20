55792c6df2ffc33f18975338a29f06a0a383c398
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2025-07-20 08:13:53 +0000

    it adds exercise of spark sql

--- src/main/scala/sparksql/SparkSql.scala
+++ src/main/scala/sparksql/SparkSql.scala
@@ -69,9 +69,59 @@ object SparkSql extends App {
     "dept_emp",
     "salaries",
     "dept_manager"
-  ))
+  ), true)
 
   // read data frame loaded Spark tables
   val employeesDataFrame = spark.read.table("employees")
   employeesDataFrame.show(false)
+
+  /**
+   * Exercises
+   *
+   * 1. Read the movies DF and store it as a Spark table in the rtjvm database.
+   * 2. Count how many employees were hired in between Jan 1 1999 and Jan 1 2000.
+   * 3. Show the average salaries for the employees hired in between those dates, grouped by department.
+   * 4. Show the name of the best-paying department for employees hired in between those dates.
+   */
+
+
+  val moviesDataFrame = spark.read
+    .option("inferSchema","true")
+    .json("src/main/scala/resources/data/movies.json")
+
+  moviesDataFrame.write.mode(SaveMode.Overwrite).saveAsTable("movies")
+  val moviesTableDataFrame = spark.read.table("movies")
+  moviesTableDataFrame.show(false)
+
+  val countedEmployeesDataFrame = spark.sql(
+    """
+      |SELECT COUNT(*) hired FROM employees WHERE hire_date BETWEEN '2000-01-01' AND '2001-01-01'
+      |""".stripMargin)
+
+  countedEmployeesDataFrame.show(false)
+
+  val averageSalaryByDepartmentDataFrame = spark.sql(
+    """
+      |SELECT AVG(salaries.salary) avg_salary, dept_emp.dept_no department_number FROM employees
+      |INNER JOIN salaries ON salaries.emp_no = employees.emp_no
+      |INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+      |WHERE employees.hire_date BETWEEN '1990-01-01' AND '2001-01-01'
+      |GROUP BY dept_emp.dept_no
+      |""".stripMargin)
+
+  averageSalaryByDepartmentDataFrame.show(false)
+
+  val bestPaidDepartment = spark.sql(
+    """
+      |SELECT AVG(salaries.salary) avg_salary, dept_emp.dept_no department_number, departments.dept_name FROM employees
+      |INNER JOIN salaries ON salaries.emp_no = employees.emp_no
+      |INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+      |INNER JOIN departments ON departments.dept_no = dept_emp.dept_no
+      |WHERE employees.hire_date BETWEEN '1990-01-01' AND '2001-01-01'
+      |GROUP BY dept_emp.dept_no, departments.dept_name
+      |ORDER BY avg_salary DESC
+      |LIMIT 1
+      |""".stripMargin)
+
+  bestPaidDepartment.show(false)
 }
